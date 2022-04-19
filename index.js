e4659e9424d1ecd4c0e0614a23478c760f784a0b
pyrotechnicsid <sidbose93@gmail.com>
2022-04-19 13:59:07 -0500

    whatever

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
   'use strict';
 
   // YOUR CODE HERE
-})();
+  var stopButton = document.getElementById('stopButton');
+  var slowButton = document.getElementById('slowButton');
+  var goButton = document.getElementById('goButton');
+  var stopLight = document.getElementById('stopLight');
+  var slowLight = document.getElementById('slowLight');
+  var goLight = document.getElementById('goLight');
+
+  var button = document.getElementsByClassName('button')
+
+  stopButton.addEventListener('click', function(){
+  if (stopLight.style.backgroundColor === 'red') {
+    stopLight.style.backgroundColor = 'black';
+  } 
+  else {
+    stopLight.style.backgroundColor = 'red';
+  }
+  
+})}())
